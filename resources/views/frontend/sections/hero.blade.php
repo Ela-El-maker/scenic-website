4e96c63a3e4637e524f087e34b8676b2b68bc93b
Ela-El-maker <feloela101@gmail.com>
2024-05-29 23:47:29 +0300

    Integrating the dashboard and the frontend scenic webiste

--- resources/views/frontend/sections/hero.blade.php
+++ resources/views/frontend/sections/hero.blade.php
@@ -1,22 +1,22 @@
-<section class="home-section section-hero overlay slanted" id="home-section">
+<section class="home-section section-hero overlay slanted" id="home-section" style="">
 
     <div class="container">
         <div class="row align-items-center justify-content-center">
             <div class="col-md-8 text-center">
-                <h1>We Are Scenic</h1>
+                <h1>{{$hero->title}}</h1>
                 <div class="mx-auto w-75">
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto quibusdam possimus quasi
-                        porro! Itaque?</p>
+                    <p>{{$hero->sub_title}}</p>
                 </div>
-                <p class="mt-5"><a href="contact.html" class="btn btn-outline-white btn-md ">Get in touch</a></p>
+                <p class="mt-5"><a href="{{$hero->btn_url}}" class="btn btn-outline-white btn-md ">{{$hero->btn_text}}</a></p>
             </div>
         </div>
     </div>
 
     <!-- VIDEO -->
     <div class="video-container">
-        <video autoplay loop="true">
-            <source type="video/mp4" src="videos/video.mp4">
+        <video autoplay loop muted playsinline id="background-video">
+            <source src="{{ asset($hero->video) }}" type="video/mp4">
+            Your browser does not support the video tag.
         </video>
     </div>
 
