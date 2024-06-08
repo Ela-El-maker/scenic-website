907f5960eaec0bd001d15908c3a5e2c06fc1ee2d
Ela-El-maker <feloela101@gmail.com>
2024-06-08 23:32:44 +0300

    Working with the admin dashboard

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -108,6 +108,20 @@
 
                 </ul>
             </li>
+
+            
+            <li class="nav-item dropdown">
+                <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i>
+                    <span>Blogs</span></a>
+                <ul class="dropdown-menu" style="display: none;">
+                    <li><a class="nav-link" href="{{ route('admin.blog-category.index') }}">Category</a></li>
+
+                    <li><a class="nav-link" href="{{ route('admin.blog-section-setting.index') }}">Blogs Title</a>
+                    </li>
+                    <li><a class="nav-link" href="{{ route('admin.blog.index') }}">Our Blogs</a></li>
+
+                </ul>
+            </li>
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
          --}}
         </ul>
